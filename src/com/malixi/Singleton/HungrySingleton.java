087795921fc1a7fa6dd4c4618bb10a65ab4279ee
malixi <692628969@qq.com>
2020-09-20 19:20:11 +0800

    饿汉式单例

--- src/com/malixi/Singleton/HungrySingleton.java
+++ src/com/malixi/Singleton/HungrySingleton.java
@@ -1,4 +1,15 @@
 package com.malixi.Singleton;
 
+/**
+ * 饿汉式单例
+ */
 public class HungrySingleton {
+    private static final HungrySingleton hungrySingleton=new HungrySingleton();
+
+    public HungrySingleton() {
+    }
+
+    public static HungrySingleton getHungrySingleton() {
+        return hungrySingleton;
+    }
 }
