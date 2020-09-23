115cfbd7b9e9e133c69c70cd2d48141e9947f89d
malixi <692628969@qq.com>
2020-09-23 13:02:03 +0800

    锁的支持

---
+++ src/com/malixi/reentrantlock/LockSupportDemo.java
@@ -0,0 +1,29 @@
+package com.malixi.reentrantlock;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * 唤醒，阻塞线程类
+ */
+public class LockSupportDemo {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+                if(i==5){
+                    //阻塞当前线程
+                    LockSupport.park();
+                }
+            }
+        });
+        t1.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 唤醒当前线程
+        LockSupport.unpark(t1);
+    }
+}
