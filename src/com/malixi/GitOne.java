52b92e0478b8088e8fa535ce1cdaf5772ae58653
malixi <692628969@qq.com>
2020-09-27 17:13:08 +0800

    实验

--- src/com/malixi/GitOne.java
+++ src/com/malixi/GitOne.java
@@ -3,10 +3,16 @@ package com.malixi;
 public class GitOne {
     public static void main(String[] args) {
         int i=10;
-        int j=2;
+        int j=10;
         int a=i%j;
 
-        System.out.println("你好");
+        System.out.println("你好1");
         System.out.println(a);
+
+        while(i==j){
+            System.out.println("nihao");
+        }
+        System.out.println("你好3");
+
     }
 }
